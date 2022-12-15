50bece52523714f27d15ca3c5e19dcc22cb2f0b6
chrisfurlong03 <chrisfurlong03@gmail.com>
2022-12-15 16:11:42 +1300

    Initial commit

---
+++ components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
+const Nav = () => (
+  <footer>
+    <div class="pb-8">
+      <div class="flex flex-wrap">
+        <div class="text-gray-400 hover:text-black pt-3">
+          <a href="/aboutsite">☞ how this portfolio was built.</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Nav;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Calistoga", "cursive"],
+        serif: ["Trocchi", "serif"]
+      },
+      maxHeight: {
+        "325": "52px"
+      }
+    }
+  },
+  plugins: []
+};
